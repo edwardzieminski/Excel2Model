008fd7255994952a961524006a58fa9e02436e61
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-20 12:54:17 +0100

    ColumnMapModel property type modified
    The properties' type was modified as it is easier to use it on further stage when it is PropertyInfo - no need to convert it back to this type.

--- Excel2Model/ColumnMapModel.cs
+++ Excel2Model/ColumnMapModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Excel2Model
 {
     public record ColumnMapModel<T>
     {
         public string ColumnName { get; init; }
-        public Func<T, IComparable> Property { get; init; }
+        public PropertyInfo Property { get; init; }
     }
 }
