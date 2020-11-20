7d9fe539ae5f22aa4bd2c829ab415fbe732ebc2f
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-20 12:49:15 +0100

    Utilities static class added
    The class contains static method GetPropertyFromExpression. It helps to extract PropertyInfo from lambda expression. PropertyInfo is needed for ColumnMapModel.

---
+++ Excel2Model/Utilities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Excel2Model
+{
+    public static class Utilities
+    {
+        public static PropertyInfo GetPropertyFromExpression<T>(Expression<Func<T, object>> GetPropertyLambda)
+        {
+            // Inspired by:
+            // https://stackoverflow.com/questions/17115634/get-propertyinfo-of-a-parameter-passed-as-lambda-expression
+            // Author: Daniel Möller
+
+            MemberExpression output;
+
+            //this line is necessary, because sometimes the expression comes in as Convert(originalexpression)
+            if (GetPropertyLambda.Body is UnaryExpression unaryExpression)
+            {
+                if (unaryExpression.Operand is MemberExpression memberExpression)
+                {
+                    output = memberExpression;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect argument. Provided unary expression is not member expression.");
+                }
+            }
+            else if (GetPropertyLambda.Body is MemberExpression memberExpression)
+            {
+                output = memberExpression;
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect argument. Provided property lambda is not member expression.");
+            }
+
+            return (PropertyInfo)output.Member;
+        }
+    }
+}
