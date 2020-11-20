1efb412322da8486826c0ebb47542934751c808f
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-20 12:56:28 +0100

    Other stuff
    Things that changed in the solution in the meantime.

--- Excel2ModelUnitTests/GeneralTest.cs
+++ Excel2ModelUnitTests/GeneralTest.cs
@@ -7,22 +7,32 @@ namespace Excel2ModelUnitTests
 {
     public class GeneralTest
     {
+        [Fact]
+        public void Is_it_working()
+        {
+            var concreteMapper = new ConcreteMapper();
+            var listOfModels = concreteMapper.GetDataFromExcel();
+            Console.WriteLine();
+        }
     }
 
     class SomeModel
     {
-        public string SomeProperty { get; set; }
+        public string KolumnaA { get; set; }
+        public string KolumnaB { get; set; }
     }
 
     class ConcreteMapper : Mapper<SomeModel>
     {
         public ConcreteMapper()
         {
-            var worksheet = new Excel.Worksheet();
+            var excelApp = new Excel.Application();
+            var workbook = excelApp.Workbooks.Open(@"C:\Users\eziemins\Desktop\Makra i inne\2020-11 listopad\Excel2Model\test.xlsx");
+            var worksheet = (Excel.Worksheet)workbook.Worksheets[0];
             Worksheet = worksheet;
-            HeaderRow = 1;
-            DataStartRow = 2;
-            AddColumn("A", someModel => someModel.SomeProperty);
+            DataStartRow = 1;
+            AddColumn("A", someModel => someModel.KolumnaA);
+            AddColumn("B", someModel => someModel.KolumnaB);
         }
     }
 }
