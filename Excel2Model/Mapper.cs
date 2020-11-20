dbeed95b637d1ce89e663959ecf36c1d10feb8cd
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-20 12:55:52 +0100

    First fully working version of Mapper class
    This version requires refactoring.

--- Excel2Model/Mapper.cs
+++ Excel2Model/Mapper.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Excel2Model
 {
-    public class Mapper<T>
+    public class Mapper<T> where T : new()
     {
         private readonly List<ColumnMapModel<T>> _columnMapModels = new List<ColumnMapModel<T>>();
 
@@ -12,15 +14,62 @@ namespace Excel2Model
         public int HeaderRow { get; init; }
         public int DataStartRow { get; init; }
 
-        public void AddColumn(string columnName, Func<T, IComparable> tProperty)
+        public void AddColumn(string columnName, Expression<Func<T, object>> tProperty)
         {
             _columnMapModels.Add(new ColumnMapModel<T>()
-            { 
+            {
                 ColumnName = columnName,
-                Property = tProperty
+                Property = Utilities.GetPropertyFromExpression(tProperty)
             });
         }
 
-        
+        public List<T> GetDataFromExcel()
+        {
+            var output = new List<T>();
+            var currentRow = DataStartRow;
+            var anyValueFulfilled = true;
+
+            do
+            {
+                var modelRecord = new T();
+                foreach (var columnMap in _columnMapModels)
+                {
+                    var cellAddress = $"{columnMap.ColumnName}{currentRow}";
+                    var cellValue = Worksheet.Range[cellAddress].Value;
+                    columnMap.Property.SetValue(modelRecord, cellValue);
+                }
+
+                anyValueFulfilled = IsAnyValueFulfilled(modelRecord);
+
+                if (anyValueFulfilled)
+                {
+                    output.Add(modelRecord);
+                }
+
+                currentRow++;
+            } while (anyValueFulfilled);
+
+            return output;
+        }
+
+        private static bool IsAnyValueFulfilled(T modelRecord)
+        {
+            var anyStringFulfilled = modelRecord.GetType().GetProperties()
+                .Where(pi => pi.PropertyType == typeof(string))
+                .Select(pi => (string)pi.GetValue(modelRecord))
+                .Any(value => string.IsNullOrEmpty(value) == false);
+
+            var anyIntDifferentThanZero = modelRecord.GetType().GetProperties()
+                .Where(pi => pi.PropertyType == typeof(int))
+                .Select(pi => (int)pi.GetValue(modelRecord))
+                .Any(value => value != 0);
+
+            var anyDoubleDifferentThanZero = modelRecord.GetType().GetProperties()
+                .Where(pi => pi.PropertyType == typeof(double))
+                .Select(pi => (double)pi.GetValue(modelRecord))
+                .Any(value => value != 0d);
+
+            return anyStringFulfilled || anyIntDifferentThanZero || anyDoubleDifferentThanZero;
+        }
     }
 }
