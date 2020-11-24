f9c2fc3a48c2891ad640baa03d3ec0da175ffc57
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-24 13:43:10 +0100

    Added static NumberNameDictionary

---
+++ Excel2Model/NumberNameDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excel2Model
+{
+    public static class NumberNameDictionary
+    {
+        private static readonly Dictionary<int, string> _nameNumberPairs;
+
+        static NumberNameDictionary()
+        {
+            _nameNumberPairs = new Dictionary<int, string>()
+            {
+                { 0, "" },
+                { 1, "A" },
+                { 2, "B" },
+                { 3, "C" },
+                { 4, "D" },
+                { 5, "E" },
+                { 6, "F" },
+                { 7, "G" },
+                { 8, "H" },
+                { 9, "I" },
+                { 10,"J" },
+                { 11, "K" },
+                { 12, "L" },
+                { 13, "M" },
+                { 14, "N" },
+                { 15, "O" },
+                { 16, "P" },
+                { 17, "Q" },
+                { 18, "R" },
+                { 19, "S" },
+                { 20, "T" },
+                { 21, "U" },
+                { 22, "V" },
+                { 23, "W" },
+                { 24, "X" },
+                { 25, "Y" },
+                { 26, "Z" }
+            };
+        }
+
+        public static int GetNumberFromName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetNameFromNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
