75f08d1d90790f34afb4531d3bc7bf64f41c21f8
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-25 15:08:40 +0100

    Added test for GetPropertyFromExpression

--- Excel2ModelUnitTests/CommonUtilitiesTests.cs
+++ Excel2ModelUnitTests/CommonUtilitiesTests.cs
@@ -5,7 +5,19 @@ namespace Excel2ModelUnitTests
 {
     public class CommonUtilitiesTests
     {
-        
+        [Fact]
+        public void GetPropertyFromExpression_provides_correct_PropertyInfo_object()
+        {
+            // ARRANGE
+            var expectedPropertyName = "TestPropertyInt";
+
+            // ACT
+            var actualPropertyInfo = CommonUtilities.GetPropertyFromExpression<TestClass>(x => x.TestPropertyInt);
+            var actualPropertyName = actualPropertyInfo.Name;
+
+            // ASSERT
+            Assert.Equal(expectedPropertyName, actualPropertyName);
+        }
     }
 
     public class TestClass
