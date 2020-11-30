a31ffa192fe9026561abc913025a6e3044e666fb
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-11-30 14:50:44 +0100

    Added ValidationError class

---
+++ Excel2Model/Validation/ValidationError.cs
@@ -0,0 +1,12 @@
+﻿namespace Excel2Model.Validation
+{
+    public class ValidationError
+    {
+        public ValidationError(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+        public string ErrorMessage { get; private init; }
+    }
+}
