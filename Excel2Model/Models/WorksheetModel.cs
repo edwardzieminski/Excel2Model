9305b9341d26d227b720a6392b7e363e68f7891b
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-01 16:21:05 +0100

    Modified WorksheetModel
    Previous version forced lib user to init all the props however lib works even with not all props initialized. I don't want to force user to enter dummy value when (s)he doesn't want to use a prop.

--- Excel2Model/Models/WorksheetModel.cs
+++ Excel2Model/Models/WorksheetModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace Excel2Model.Models
 {
-    public record WorksheetModel (string WorkbookPath, string WorksheetName, int WorksheetIndex, int HeaderRow, int DataStartRow)
+    public record WorksheetModel
     {
-        
+        public string WorkbookPath { get; init; }
+        public string WorksheetName { get; init; }
+        public int WorksheetIndex { get; init; }
+        public int HeaderRow { get; init; }
+        public int DataStartRow { get; init; }
     }
 }
