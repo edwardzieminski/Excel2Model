55e2c699c50d6c4fff5e4594904398898534fff9
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-01 17:21:14 +0100

    Implemented AbstractMapper

--- Excel2Model/Mappers/TableHeaderMapper.cs
+++ Excel2Model/Mappers/TableHeaderMapper.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Excel2Model.Models;
+using Excel2Model.Validation;
+using Microsoft.Office.Interop.Excel;
+using Optional;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Excel2Model.Mappers
 {
-    class TableHeaderMapper
+    public class TableHeaderMapper<T> : AbstractMapper<T> where T : new()
     {
+        public Option<ColumnMapModel<T>, ValidationError> TryAddColumn(string columnHeader, Expression<Func<T, object>> tProperty)
+        {
+            throw new NotImplementedException();
+        }
+
+        private protected override List<T> GetDataFromExcelInterop(Worksheet excelInteropWorksheet)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
