04c55f4a1682a0b3e94f3abc24a83a401f1f4638
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-01 17:01:37 +0100

    Adapted ColumnNameMapper to AbstractMapper
    Now ColumnNameMapper derives from AbstractMapper so previously moved methods are now removed from ColumnNameMapper.

--- Excel2Model/Mappers/ColumnNameMapper.cs
+++ Excel2Model/Mappers/ColumnNameMapper.cs
@@ -6,42 +6,12 @@ using Optional;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
-using System.Reflection;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Excel2Model.Mappers
 {
-    public class ColumnNameMapper<T> where T : new()
+    public class ColumnNameMapper<T> : AbstractMapper<T> where T : new()
     {
-        private readonly List<ColumnMapModel<T>> _columnMapModels = new List<ColumnMapModel<T>>();
-        private Option<Excel.Worksheet, ValidationError> _excelInteropWorksheetOrValidationError;
-        private WorksheetModel _worksheetModel;
-
-        public ColumnNameMapper()
-        {
-        }
-
-        public ColumnNameMapper(WorksheetModel worksheetModel)
-        {
-            WorksheetModel = worksheetModel;
-        }
-
-        public WorksheetModel WorksheetModel
-        {
-            get => _worksheetModel;
-            init
-            {
-                _worksheetModel = value;
-                SetExcelInteropWorksheet();
-            }
-        }
-
-        private void SetExcelInteropWorksheet()
-        {
-            _excelInteropWorksheetOrValidationError = ExcelInteropUtilities.TryGetWorksheet(WorksheetModel);
-        }
-
-        public Option<ColumnMapModel<T>, ValidationError> TryAddColumn(string columnName, Expression<Func<T, object>> tProperty)
+        public override Option<ColumnMapModel<T>, ValidationError> TryAddColumn(string columnName, Expression<Func<T, object>> tProperty)
         {
             Option<ColumnMapModel<T>, ValidationError> output = new Option<ColumnMapModel<T>, ValidationError>();
 
@@ -56,45 +26,7 @@ namespace Excel2Model.Mappers
             return output;
         }
 
-        private ColumnMapModel<T> AddColumn(string columnName, PropertyInfo propertyInfo)
-        {
-            var output = new ColumnMapModel<T>()
-            {
-                ColumnName = columnName,
-                Property = propertyInfo
-            };
-
-            _columnMapModels.Add(output);
-
-            return output;
-        }
-
-        public Option<List<T>, ValidationError> TryGetDataFromExcel()
-        {
-            Option<List<T>, ValidationError> output = new Option<List<T>, ValidationError>();
-
-            _excelInteropWorksheetOrValidationError.Match
-            (
-                some: excelInteropWorksheet => output = Option.Some<List<T>, ValidationError>(GetDataFromExcelInterop(excelInteropWorksheet)),
-                none: validationError => output = Option.None<List<T>, ValidationError>(validationError)
-            );
-
-            return output;
-        }
-
-        public List<T> TryGetDataFromExcelWithoutErrors()
-        {
-            List<T> output = new List<T>();
-
-            _excelInteropWorksheetOrValidationError.MatchSome
-            (
-                some: excelInteropWorksheet => output = GetDataFromExcelInterop(excelInteropWorksheet)
-            );
-
-            return output;
-        }
-
-        private List<T> GetDataFromExcelInterop(Worksheet excelInteropWorksheet)
+        private protected override List<T> GetDataFromExcelInterop(Worksheet excelInteropWorksheet)
         {
             var output = new List<T>();
 
