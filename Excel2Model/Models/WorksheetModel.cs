b6c31b5a8119200ce780d1bb4010956560ca6673
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-01 10:34:41 +0100

    Added WorksheetModel

---
+++ Excel2Model/Models/WorksheetModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Excel2Model.Models
+{
+    public record WorksheetModel (string WorkbookPath, string WorksheetName, int WorksheetIndex, int HeaderRow, int DataStartRow)
+    {
+        
+    }
+}
