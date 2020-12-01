6c8138042dfbf9f90d9c8e131a5d9b35876feaff
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-01 10:34:19 +0100

    Added FileUtilities

---
+++ Excel2Model/Utilities/FilesUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Excel2Model.Utilities
+{
+    public static class FilesUtilities
+    {
+        public static bool FileExists(string filePath)
+        {
+            try
+            {
+                Path.GetFullPath(filePath);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
