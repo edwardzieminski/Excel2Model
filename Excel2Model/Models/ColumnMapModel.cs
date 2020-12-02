c2eec516a37754f6eab34e6e4cac3d09ae0232f1
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-02 11:46:04 +0100

    Added new properties
    New props might be useful for other mapper types than ColumnNameMapper.

--- Excel2Model/Models/ColumnMapModel.cs
+++ Excel2Model/Models/ColumnMapModel.cs
@@ -5,6 +5,8 @@ namespace Excel2Model.Models
     public record ColumnMapModel<T>
     {
         public string ColumnName { get; init; }
+        public int ColumnIndex { get; init; }
+        public string ColumnHeader { get; init; }
         public PropertyInfo Property { get; init; }
     }
 }
