fcb1ad271a2255d01e403f80f5918b9c716a5d20
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-03 15:22:32 +0100

    Added CouldNotResolveMapException

---
+++ Excel2Model/Validation/CouldNotResolveMapException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel2Model.Validation
+{
+    public class CouldNotResolveMapException : Exception
+    {
+        public CouldNotResolveMapException()
+        {
+
+        }
+    }
+}
