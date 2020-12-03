969916d7d55bc1e6061c7b027c3a309a0d5e92b9
Edward Zieminski <Edward.Zieminski@dhl.com>
2020-12-03 15:39:14 +0100

    Added ColumnMapValidator

---
+++ Excel2Model/Validation/ColumnMapValidator.cs
@@ -0,0 +1,10 @@
+﻿using Excel2Model.Models;
+using FluentValidation;
+
+namespace Excel2Model.Validation
+{
+    public class ColumnMapValidator : AbstractValidator<ColumnMapModel>
+    {
+        
+    }
+}
